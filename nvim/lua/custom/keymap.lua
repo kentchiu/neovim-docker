0cef98b3790bc78ccf680346e50db8ca7cf11a1c
Kent Chiu <kent.cwg@gmail.com>
2023-03-04 15:29:37 +0800

    terminal

--- nvim/lua/custom/keymap.lua
+++ nvim/lua/custom/keymap.lua
@@ -171,6 +171,10 @@ map("n", "<leader>|", "<C-W>v", { desc = "Split window right" })
 -- map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
 -- map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
 
+
+-- 
+-- lazygit 
+--
 local Terminal  = require('toggleterm.terminal').Terminal
 local lazygit = Terminal:new({
   cmd = "lazygit",
@@ -190,16 +194,41 @@ local lazygit = Terminal:new({
   end,
 })
 
-function _lazygit_toggle()
+function lazygit_toggle()
   lazygit:toggle()
 end
 
-function _lazygit_toggle()
-  lazygit:toggle()
+
+vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua lazygit_toggle()<CR>", {noremap = true, silent = true, desc = "lazygit"})
+
+
+-- 
+-- terminal 
+--
+
+local terminal = Terminal:new({
+  cmd = "zsh",
+  direction = "float",
+  float_opts = {
+    --    border = 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
+    border = "curved",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+  -- function to run on closing the terminal
+  on_close = function(term)
+    vim.cmd("startinsert!")
+  end,
+})
+
+function terminal_toggle()
+  terminal:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true, desc = "lazygit"})
+
+vim.api.nvim_set_keymap("n", "<leader>ft", "<cmd>lua terminal_toggle()<CR>", {noremap = true, silent = true, desc = "Terminal"})
 
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et`
